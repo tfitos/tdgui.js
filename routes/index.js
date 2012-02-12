150123aad644092a6ce4b48da41caebb67b381a6
tamas.fitos <tamas.fitos@gmail.com>
2012-02-12 17:15:38 +0100

    introduced async to better control flow

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var persistence = require('../tdpersistence');
+var async = require('async');
 
 // e.g. 2012-FEB-05
 var DATEEXP = /^\d{4}-\w{3}-\d{2}$/;
@@ -24,38 +25,53 @@ exports.index = function(req, res){
 exports.tdlist = function(req, res){
     console.log("Request: " + req.params.date);
     var valid = DATEEXP.exec(req.params.date);
+    var doc, nextDate, prevDate;
     if(valid){
-        persistence.findByDate(req.params.date, function(err, doc){
-            if(err) throw err;
+        async.series([
+            function(callback){
+                persistence.findByDate(req.params.date, function(err, docu){
+                    if(err) throw err;
 
-            var prevDate, nextDate;
-
-            console.dir(doc);
-            if(doc){
+                    var prevDate, nextDate;
+                    console.dir(docu);
+                    if(docu){
+                        doc = docu;
+                        callback(null);
+                    }else{
+                        res.render('missing', {title: "Missing TD list", d: req.params.date});
+                    }
+                });
+            },
+            function(callback){
                 persistence.findPrevByRealDate(doc.realdate, function(err1, prev){
                     if(err1) throw err1;
                     if(prev){
                         console.dir("Prev date:" + prev.date);
                         prevDate = prev.date;
                     }
-                    persistence.findNextByRealDate(doc.realdate, function(err2, next){
-                        if(err2) throw err2;
-                        if(next){
-                            console.dir("Next date:" + next.date);
-                            nextDate = next.date;
-                        }
-                        res.render('tdlist', {tdlist : doc,
-                                                title: doc.date,
-                                                nextDate: nextDate,
-                                                prevDate: prevDate});
-                    });
+                    callback(null);
+                });
+            },
+            function(callback){
+                persistence.findNextByRealDate(doc.realdate, function(err2, next){
+                    if(err2) throw err2;
+                    if(next){
+                        console.dir("Next date:" + next.date);
+                        nextDate = next.date;
+                    }
+                    callback(null);
 
                 });
-
-            }else{
-                res.render('missing', {title: "Missing TD list", d: req.params.date});
+            },
+            function(callback){
+                res.render('tdlist', {tdlist : doc,
+                    title: doc.date,
+                    nextDate: nextDate,
+                    prevDate: prevDate});
+                callback(null);
             }
-        });
+        ]);
+
     }else{
         res.render('invalid', {title: "Invalid request"});
     }
